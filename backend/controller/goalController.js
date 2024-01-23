7e16c6db062e59c7ef4cd488a5f0eaa72e46c490
NyashaMuusha <nyashamuusha@gmail.com>
2024-01-23 06:52:22 +0200

    added goal controller

---
+++ backend/controller/goalController.js
@@ -0,0 +1,40 @@
+//@desc Get Goals
+//@route GET /api/goals
+//@access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'Get goals' });
+}
+
+//@desc Set Goals
+//@route POST /api/goals
+//@access Private
+const setGoal = (req, res) => {
+  if (!req.body || !req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+
+  } else {
+    // Access the text property when it's guaranteed to exist
+    const goalText = req.body.text;
+    // Your logic for processing the goal text
+
+    res.status(200).json({ message: 'Set goal' });
+  }
+};
+
+//@desc Update Goals
+//@route PUT /api/goals/:id
+//@access Private
+const updateGoal = (req, res) => {
+  res.status(400).json({ message: `Updated  goal ${req.params.id}` });
+}
+
+//@desc Delete Goals
+//@route DELETE /api/goals/:id
+//@access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Get goals ${req.params.id}` });
+}
+
+
+module.exports = { getGoals, setGoal, updateGoal, deleteGoal }
